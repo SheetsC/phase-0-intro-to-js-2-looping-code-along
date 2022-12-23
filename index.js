f85b094163beb21a7b6931f0d98d4bdb84bd790f
Connor Sheets <connorsheets32@gmail.com>
2022-12-22 23:15:52 -0600

    commit tree acd10dc97be0ac29e586a20f74f46cb1de192152

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+
+function writeCards(names, event) {
+    const cards = []
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        cards.push(message)
+
+    }
+    return cards
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+// function wrapGifts(gift){
+//     let i = 0
+//     while (i < gifts.length) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow`);
+//         i++
+//     }
+//     return gift
+// }
+// wrapGifts(gifts)
+
+function countDown(){
+    let count = 10
+    while (count >= 0) console.log(count--)
+}
+
+countDown(4)
